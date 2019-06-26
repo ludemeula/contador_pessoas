b4c0b08f3889322b32da206560c1431a3f87e3a0
ludemeula <ludemeula@gmail.com>
2019-06-26 15:01:29 -0300

    Adding stateless

--- lib/main.dart
+++ lib/main.dart
@@ -4,45 +4,72 @@ void main() {
   runApp(MaterialApp(
       title: 'Contador de Pessoas',
       //home: Container(color: Colors.amberAccent,),
-      home: Stack(children: <Widget>[
-        Image.asset(
-          'images/restaurant.jpg',
-          fit: BoxFit.cover,
-          height: 1000.0,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Pessoas: 0',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      '+1',
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    )),
-                FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      '-1',
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    ))
-              ],
-            ),
-            Text(
-              'Pode Entrar!',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0),
-            )
-          ],
-        )
-      ])));
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = 'Pode Entrar!';
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      _infoText = _people <= 0 ? 'Mundo Invertido!' : (_people <= 10 ? 'Pode Entrar!' : 'Lotado!');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Image.asset(
+        'images/restaurant.jpg',
+        fit: BoxFit.cover,
+        height: 1000.0,
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Pessoas: $_people',
+            style:
+            TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    _changePeople(1);
+                  },
+                  child: Text(
+                    '+1',
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  )),
+              FlatButton(
+                  onPressed: () {
+                    _changePeople(-1);
+                  },
+                  child: Text(
+                    '-1',
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ))
+            ],
+          ),
+          Text(
+            _infoText,
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
+      )
+    ]);
+  }
 }
