7f5e53dd6d315830651a4a1a007127a31b2d96b7
ludemeula <ludemeula@gmail.com>
2019-06-26 14:42:25 -0300

    Adding image to the main page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Contador de Pessoas',
-    home: Container(color: Colors.white,)
-  ));
+      title: 'Contador de Pessoas',
+      //home: Container(color: Colors.amberAccent,),
+      home: Stack(children: <Widget>[
+        Image.asset(
+          'images/restaurant.jpg',
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pessoas: 0',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      '+1',
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    )),
+                FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      '-1',
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ))
+              ],
+            ),
+            Text(
+              'Pode Entrar!',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ])));
 }
